e36b626823656f4031d1a91ec42c0c8791b792b3
Kris Ezra <kris.ezra@gmail.com>
2022-10-12 12:56:36 -0400

    Code looks pretty good for the main run of ddpg now

--- main.py
+++ main.py
@@ -18,14 +18,23 @@ env = UnityEnvironment(file_name="Tennis_Linux/Tennis.x86_64")
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
 
-# Set up the agent generically for state and action sizes.  Can't ever be TOO
-# portable!
-# TODO is there a way to see how many agents there are? Generically?  Need to put some generic looping in here.
-agent1 = Agent(state_size=brain.vector_observation_space_size,
-               action_size=brain.vector_action_space_size)
 
-agent2 = Agent(state_size=brain.vector_observation_space_size,
-               action_size=brain.vector_action_space_size, memory=agent1.memory)
+n_agents = len(env.reset(train_mode=True)[brain_name].agents)
+
+agents = []
+
+# Set up the agents generically for state and action sizes and number of agents
+for i in range(n_agents):
+    # Create the agents and let them share a replay buffer
+    if agents:
+        memory = None
+    else:
+        memory = agents[-1].memory
+
+    new_agent = Agent(state_size=brain.vector_observation_space_size,
+                      action_size=brain.vector_action_space_size,memory=memory)
+
+    agents.append(new_agent)
 
 # Train the agent
 def ddpg(n_episodes=6500, max_t=1000):
@@ -36,58 +45,72 @@ def ddpg(n_episodes=6500, max_t=1000):
         n_episodes (int): maximum number of training episodes
         max_t (int): maximum number of timesteps per episode
     """
-    scores = []  # list containing scores from each episode
+    # Initialize the list of episode max scores
+    scores = []
+
+    # Initialize a moving average of last 100 scores
     scores_window = deque(maxlen=100)  # last 100 scores
+
     for i_episode in range(1, n_episodes+1):
         # Reset the environment in training mode according to the standard
         # brain
         env_info = env.reset(train_mode=True)[brain_name]
-        num_agents = len(env_info.agents)
 
         # Get the states from the environment
         states = env_info.vector_observations
 
         # Get the agents ready for this new episode
-        agent1.reset()
-        agent2.reset()
+        for agent in agents:
+            agent.reset()
 
-        # Initialize the score to zero
-        score = np.zeros(2)
+        # Initialize the episode score to zero for each agent
+        agent_scores = np.zeros(n_agents)
         # We will leave a maximum time in here for now
         for _ in range(max_t):
-            state1 = states[0]
-            state2 = states[1]
-            action1 = agent1.act(state1)
-            action2 = agent2.act(state2)
-            actions = np.stack((action1,action2))
-            env_info = env.step(actions)[brain_name]
+            # Go get the actions for each agent, given the current state
+            actions = []
+            for agent, state in zip(agents,states):
+                action = agent.act(state)
+                actions.append(action)
+
+            # Put all the states into a list of lists and feed it to the
+            # environment
+            env_info = env.step(np.stack(actions))[brain_name]
             next_states = env_info.vector_observations
             rewards = env_info.rewards
             dones = env_info.local_done
-            agent1.step(state1, action1, rewards[0], next_states[0], dones[0])
-            agent2.step(state2, action2, rewards[1], next_states[1], dones[0])
+
+            # Do the next step for all agents
+            for agent, state, action, reward, next_state, done in zip(
+                    agents, states, actions, rewards, next_states, dones):
+                agent.step(state, action, reward, next_state, done)
+
+            # Increment the state for the next loop
             states = next_states
-            score += rewards
+            agent_scores += rewards
             if np.any(dones):
                 break
-        max_score = np.max(score)
-        scores_window.append(max_score)       # save most recent max score
-        scores.append(max_score)              # save most recent score
+        # save most recent episode score in moving average and to the complete list
+        # Recall that the episode score is the maximum score of any agent
+        episode_score = np.max(agent_scores)
+        scores_window.append(episode_score)
+        scores.append(episode_score)
 
         if i_episode % 100 == 0:
             print(
                     '\rEpisode {}\tAverage Score: {:.2f}'.format(
                         i_episode, np.mean(scores_window)))
 
-        if np.mean(scores_window) >= 30:
+        if np.mean(scores_window) >= 0.5:
             print(
                     '\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
                         i_episode-100, np.mean(scores_window)))
             # If we win, we need to save the checkpoint
-            torch.save(agent1.actor_local.state_dict(), 'checkpoint-actor1.pth')
-            torch.save(agent1.critic_local.state_dict(), 'checkpoint-critic1.pth')
-            torch.save(agent2.actor_local.state_dict(), 'checkpoint-actor2.pth')
-            torch.save(agent2.critic_local.state_dict(), 'checkpoint-critic2.pth')
+            for i,agent in enumerate(agents):
+                torch.save(agent.actor_local.state_dict(),
+                           F'checkpoint-actor{i}.pth')
+                torch.save(agent.critic_local.state_dict(),
+                           F'checkpoint-critic{i}.pth')
             break
 
     # Close the environment
